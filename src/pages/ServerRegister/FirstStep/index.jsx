7ceaa2de5e2fdbc4e92f48393428069893821c34
Gabriel Castro <gabrielcssg@gmail.com>
2020-06-02 00:35:31 -0300

    Atualização de usuario servidor, firstStep

--- src/pages/ServerRegister/FirstStep/index.jsx
+++ src/pages/ServerRegister/FirstStep/index.jsx
@@ -2,22 +2,32 @@ import React from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../../hooks/auth';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import { Container, Title, Buttons } from './styles';
 import { View } from 'react-native';
 import Button from '../../../components/button';
 
+import api from '../../../services/api';
+
 const FirstStep = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
   const navigation = useNavigation();
 
   const handleVisitor = () => {
     navigation.navigate('Dashboard');
   };
 
-  const handleServer = () => {
-    // criar logica do servidor
-    // navigation.navigate('SecondStep');
+  const handleServer = async () => {
+    user.isGovernmentEmployee = true;
+    const token = await AsyncStorage.getItem('@Permutas:token');
+
+    await api.put('users', user , {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    navigation.navigate('Dashboard');
   }
 
   return (

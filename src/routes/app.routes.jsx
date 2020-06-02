cb3ccb5049695a7ce7ebe9a95d722feffd4cc7da
matheus <noronhamatheus07@gmail.com>
2020-06-02 19:54:41 -0300

    tab navigator

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Dashboard from '../pages/Dashboard';
 import ListInstitutions from '../pages/ListInstitutions'
@@ -8,21 +9,61 @@ import FirstStep from '../pages/ServerRegister/FirstStep';
 import CargoRegister from '../pages/CargoRegister';
 
 const App = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+function HomeTabScreen() {
+  return (
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            if (route.name === 'Dashboard') {
+              return (
+                <MaterialCommunityIcons
+                  name={
+                    focused
+                      ? 'home'
+                      : 'home-outline'
+                  }
+                  size={size}
+                  color={color}
+                />
+              );
+            } else if (route.name === 'Interest') {
+              return (
+                <Ionicons
+                  name={focused ? 'ios-list-box' : 'ios-list'}
+                  size={size}
+                  color={color}
+                />
+              );
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#000',
+          inactiveTintColor: 'gray',
+        }}
+      >
+      <Tab.Screen name="Dashboard" component={Dashboard}/>
+      <Tab.Screen name="Interest" component={ListInstitutions} />
+    </Tab.Navigator>
+  )
+}
 
 const AppRoutes = () => {
   return (
-    <App.Navigator
-      screenOptions={{
-        headerShown: false,
-        cardStyle: { backgroundColor: '#7c60f7' },
-      }}
-    >
-
-      <App.Screen name="Dashboard" component={Dashboard} />
-      <App.Screen name="ListInstitutions" component={ListInstitutions} />
-      <App.Screen name="FirstStep" component={FirstStep} />
-      <App.Screen name="CargoRegister" component={CargoRegister} />
-    </App.Navigator>
+      <App.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: '#7c60f7' },
+        }}
+      >
+        <App.Screen name="Home" component={HomeTabScreen} />
+        <App.Screen name="ListInstitutions" component={ListInstitutions} />
+        <App.Screen name="FirstStep" component={FirstStep} />
+        <App.Screen name="CargoRegister" component={CargoRegister} />
+      </App.Navigator>
   );
 };
 

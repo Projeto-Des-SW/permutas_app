9a98d58e69518333744c07cddabccf9e91a8596a
Gabriel Castro <gabrielcssg@gmail.com>
2020-06-07 18:57:55 -0300

    consumo da listagem de matchs

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import Axios from 'axios';
 
 const api = Axios.create({
-  baseURL: 'http://localhost:3333',
-  // baseURL: 'http://192.168.1.9:3333'
+  baseURL: 'http://192.168.1.11:3333',
+  // baseURL: 'https://permutaservidor.herokuapp.com/'
 });
 
 export default api;

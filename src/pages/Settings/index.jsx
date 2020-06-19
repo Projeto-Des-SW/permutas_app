2fc3115f017239201f6b563308014761080caee4
Alessandro <alessandrojr1998@gmail.com>
2020-06-19 15:17:46 -0300

    limpeza no código da tela de configurações

--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -1,33 +1,13 @@
-import React, { useRef, useCallback, useEffect, useState } from 'react';
-import DropDown from '../../components/dropDown';
+import React from 'react';
 
 import {
-  Image,
-  ScrollView,
   View,
-  KeyboardAvoidingView,
-  Platform,
-  TextInput,
-  Alert,
 } from 'react-native';
 
-import { Container, Title, BackToSign, BackToSignText, PickerSelectStyles } from './styles';
+import { Container } from './styles';
 
 import Button from '../../components/button';
-import Input from '../../components/input';
-import Keyboard from '../../components/keyboard';
-
-import { useNavigation } from '@react-navigation/native';
-import { Feather } from '@expo/vector-icons';
-import * as Yup from 'yup';
-
-import { Form } from '@unform/mobile';
-
 import { useAuth } from '../../hooks/auth';
-
-import getValidationErrors from '../../utils/getValidationErros';
-import api from '../../services/api';
-import apiIbge from '../../services/apiIBGE';
 
 const Settings = () => {
   const { signOut, user } = useAuth();

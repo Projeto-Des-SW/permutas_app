2282ad89b59968e1e4af4196da6ce0a60c94b8c5
Gabriel Castro <gabrielcssg@gmail.com>
2020-10-20 21:48:20 -0300

    feat: servidor de produçao

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import Axios from 'axios';
 
 const api = Axios.create({
   // baseURL: 'http://localhost:3333',
-  baseURL: 'http://192.168.1.11:3333'
-  // baseURL: 'https://permutaservidor.herokuapp.com/'
+  // baseURL: 'http://192.168.1.11:3333'
+  baseURL: 'https://permutaservidor.herokuapp.com/'
 });
 
 export default api;
